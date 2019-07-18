553c5974210d1313a95474020f6085f58c562286
mlnenicek <mihael.lnenicek@gmail.com>
2019-07-18 13:13:22 +0200

    Added class  in Service and Service.Common

--- AHP/AHP.Service/ProjectService.cs
+++ AHP/AHP.Service/ProjectService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AHP.Service.Common
+namespace AHP.Service
 {
-    public class Class1
+    class ProjectService
     {
     }
 }
