bcc9212adbd600347b43339b2d0648cb41094bea
Lucija Tatarević <lucija.tatarevic@gmail.com>
2019-07-22 11:39:09 +0200

    Add ResultController

---
+++ AHP/AHP/Controllers/ResultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AHP.Controllers
+{
+    public class ResultController : Controller
+    {
+        // GET: Result/FinalResult
+        public ActionResult FinalResult()
+        {
+
+            return View();
+        }
+    }
+}
