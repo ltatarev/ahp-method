977af89eb8ffbd31fcd5ea744f655916fb9d4da0
JakovSudar <jakovsudar1@gmail.com>
2019-07-23 11:27:28 +0200

    Project service added

--- AHP/AHP.Service.Common/IProjectService.cs
+++ AHP/AHP.Service.Common/IProjectService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AHP.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,9 @@ namespace AHP.Service.Common
 {
     public interface IProjectService
     {
+        Task<List<Project>> GetProjectsAsync(int PageNumber);
+        Task<Project> GetProjectByIdAsync(int ProjectId);
+        Task<Project> AddProjectAsync(Project project);      
+        
     }
 }
