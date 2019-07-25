06660cd5bb84fef199d934fa62c4722753750e8e
Lucija Tatarević <lucija.tatarevic@gmail.com>
2019-07-25 13:52:09 +0200

    Add dynamic form to Editcriteria

---
+++ AHP/AHP/Models/CriteriaRankView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AHP.Models
+{
+    public class CriteriaRankView
+    {
+        public int Criteria1Id { get; set; }
+        public int Column { get; set; }
+        public float Priority { get; set; }
+
+    }
+}
