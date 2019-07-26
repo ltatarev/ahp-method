77ef4e54a748cea98838360b700320041edebfb0
JakovSudar <jakovsudar1@gmail.com>
2019-07-26 08:46:40 +0200

    criterias by projectID sent to view

--- AHP/AHP/Controllers/CriterionController.cs
+++ AHP/AHP/Controllers/CriterionController.cs
@@ -55,12 +55,13 @@ namespace AHP.Controllers
         public async Task<ActionResult> EditCriteria(int id)
         {
             // TO DO: GET all Criteria for certain ProjectId
+            
             // IList<CriterionView> Criteria = new List<CriterionView>();
             // projectId??
-
             var CriteriasInProject = await CriteriaService.GetCriteriasByProjectId(id, 1);
+            var criteriaView = _mapper.Map<List<CriterionView>>(CriteriasInProject);
 
-            return View();
+            return View(criteriaView);
         }
 
         // POST: Criterion/EditCriterionPreference
